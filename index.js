0ec92933933f668260ebbd232e364f7a9ae04fff
Gerald <gerald.hofmaier@gmail.com>
2015-03-17 17:47:44 +0100

    Make code more object orientated

--- index.js
+++ index.js
@@ -8,78 +8,88 @@ if (argv.h || argv.help) {
   process.exit(code=0);
 }
 
-// Default location
-// TODO specify this in a config file
-var location = 'falmouth';
-var date = '';
+function TideCrawler(argv) {
 
+  //inherit from node-crawler
+  Crawler.call(this);
 
-// if the location argument starts with @ remove it
-var formatLocation = function(location) {
-  if (location[0] === '@') {
-    return location.substring(1)
-  } else {
-    return location
-  }
-}
+  this.location = 'falmouth';  //Default location
+  this.date = '';
 
-// takes tomorrow or a day of the week and converts it to a date url fragment 
-var formatDate = function(day) {
-  // Vaid Day arguments
-  var dayArgs = ['tomorrow', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-
-  var index = dayArgs.indexOf(day);
-  if (index > -1 ){
-    if (index === 0) {
-      var date = moment().add('days', 1).format('-YYYYMMDD');
+  // if the location argument starts with @ remove it
+  this.formatLocation = function(location) {
+    if (location[0] === '@') {
+      return location.substring(1)
     } else {
-      var date = moment().day(index).format('-YYYYMMDD');
+      return location
     }
-    return date;
-  } 
-}
-
-if (argv._.length == 1) {
-  // is the argument a date or a location?
-  var d = formatDate(argv._[0])
-  if (typeof(d) != "undefined"){
-    date = d
-  } else {
-    location = formatLocation(argv._[0])
   }
-} else if (argv._.length == 2) {
-  // two arguments provided
-  // first argument should be date related
-  // second argument shoudl be a location
-  date = formatDate(argv._[0])
 
-  location = formatLocation(argv._[1])
-}
+  // takes tomorrow or a day of the week and converts it to a date url fragment 
+  this.formatDate = function(day) {
+    // Vaid Day arguments
+    var dayArgs = ['tomorrow', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
-// the uri of page where we will scrape the data
-var uri = 'http://www.tidetimes.co.uk/'+location+'-tide-times'+date
+    var index = dayArgs.indexOf(day);
+    if (index > -1 ){
+      if (index === 0) {
+        var date = moment().add('days', 1).format('-YYYYMMDD');
+      } else {
+        var date = moment().day(index).format('-YYYYMMDD');
+      }
+      return date;
+    } 
+  }
 
-// Format the scaped tide times by removing extra spaces, adding a line break after m, and removing the leading spaces
-formatTideTimes = function(times) {
-  return times.replace(/\s+/g, " ").replace(/m /g, "m\n").substring(1)
-}
+  // Format the scaped tide times by removing extra spaces, adding a line break after m, and removing the leading spaces
+  this.formatTideTimes = function(times) {
+    return times.replace(/\s+/g, " ").replace(/m /g, "m\n").substring(1)
+  }
 
-
-// Scrape the data
-
-var c = new Crawler({
-"maxConnections":1,
-
-"callback":function(error,result,$) {
-    
-    if(!error && result.statusCode === 200){
-      var times = $(".times").text();
-      console.log(formatTideTimes(times));
-    }else{
-      console.log('No data available for this location')
+  //Set location and date via values passed from args
+  if (argv._.length == 1) {
+    // is the argument a date or a location?
+    var d = this.formatDate(argv._[0])
+    if (typeof(d) != "undefined"){
+      this.date = d
+    } else {
+      location = this.formatLocation(argv._[0])
     }
-    process.exit(code=0);
-}
-});
+    } else if (argv._.length == 2) {
+    // two arguments provided
+    // first argument should be date related
+    // second argument shoudl be a location
+    this.date = this.formatDate(argv._[0])
 
-c.queue(uri);
+    this.location = this.formatLocation(argv._[1])
+  }
+
+  // the uri of page where we will scrape the data
+  this.uri = 'http://www.tidetimes.co.uk/'+this.location+'-tide-times'+this.date;
+
+
+  this.getTimes = function(){
+
+    // Scrape the data
+    var c = new Crawler({
+    "maxConnections":1,
+
+    "callback":function(error,result,$) {
+        
+        if(!error && result.statusCode === 200){
+          return $(".times").text();
+        }else{
+          return 'No data available for this location'
+        }
+    }
+    });
+
+    // !!!! Need to call formatTideTimes after c.queue completes !!!!        
+    this.formatTideTimes(c.queue(this.uri));
+  }
+
+
+}
+
+var tides = new TideCrawler(argv);
+console.log(tides.getTimes());
