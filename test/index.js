150e2234c3b89956783c5a8301de012432aa5d51
Gerald <gerald.hofmaier@gmail.com>
2015-09-07 13:01:25 +0200

    Fix problem reading test data

--- test/index.js
+++ test/index.js
@@ -1,39 +1,10 @@
 var TideCrawler = require('../lib/index.js');
 var should = require('chai').should();
+var fs = require('fs');
 
-var tideData = "
-
-                                  High Tide:
-                                03:05
-                Height:
-                3.90m
-
-
-                                  Low Tide:
-                                09:25
-                Height:
-                1.80m
-
-
-                                  High Tide:
-                                15:58
-                Height:
-                3.80m
-
-
-                                  Low Tide:
-                                22:18
-                Height:
-                1.90m
-
-                          "
-
-var tideDataAsText = "High Tide: 03:05 Height: 3.90m
-Low Tide: 09:25 Height: 1.80m
-High Tide: 15:58 Height: 3.80m
-Low Tide: 22:18 Height: 1.90m"
-
-var tideDataAsJSON = "JSON DATA TO GO HERE"
+var tideData = fs.readFileSync('./test/test_data.txt').toString();
+var tideDataAsText = "High Tide: 03:05 Height: 3.90m\nLow Tide: 09:25 Height: 1.80m\nHigh Tide: 15:58 Height: 3.80m\nLow Tide: 22:18 Height: 1.90m";
+var tideDataAsJSON  = "";
 
 
 describe('format tide data', function(){
