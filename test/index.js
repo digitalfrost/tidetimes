2682f910df75b7b6d294ccde4ef204366942e724
Gerald <gerald.hofmaier@gmail.com>
2015-09-08 22:45:24 +0200

    test formatToText

--- test/index.js
+++ test/index.js
@@ -3,14 +3,14 @@ var should = require('chai').should();
 var fs = require('fs');
 
 var tideData = fs.readFileSync('./test/test_data.txt').toString();
-var tideDataAsText = "High Tide: 03:05 Height: 3.90m\nLow Tide: 09:25 Height: 1.80m\nHigh Tide: 15:58 Height: 3.80m\nLow Tide: 22:18 Height: 1.90m";
+var tideDataAsText = "High Tide: 03:05 Height: 3.90m\nLow Tide: 09:25 Height: 1.80m\nHigh Tide: 15:58 Height: 3.80m\nLow Tide: 22:18 Height: 1.90m\n";
 var tideDataAsJSON  = "";
 
 
 describe('format tide data', function(){
   it('should format tide data as text', function(){
       tides = new TideCrawler();
-      tides.formatToText(tideData).should.equal(tideDataAsText)
+      tides._formatToText(tideData).should.equal(tideDataAsText)
   });
   it('should format tide data as JSON', function(){
       tides = new TideCrawler();
