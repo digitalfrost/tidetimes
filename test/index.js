77467988790c9b0f385ab1e3e934b6cb18d0c36f
Gerald <gerald.hofmaier@gmail.com>
2015-09-08 23:42:55 +0200

    format tide data as json

--- test/index.js
+++ test/index.js
@@ -4,7 +4,9 @@ var fs = require('fs');
 
 var tideData = fs.readFileSync('./test/test_data.txt').toString();
 var tideDataAsText = "High Tide: 03:05 Height: 3.90m\nLow Tide: 09:25 Height: 1.80m\nHigh Tide: 15:58 Height: 3.80m\nLow Tide: 22:18 Height: 1.90m\n";
-var tideDataAsJSON  = "";
+var tideDataAsJSON  = { low: [ { time: '09:25', hight: '1.80' }, { time: '22:18', hight: '1.90' } ],
+                        high: [ { time: '03:05', hight: '3.90' },{ time: '15:58', hight: '3.80' } ] }
+;
 
 
 describe('format tide data', function(){
@@ -14,7 +16,7 @@ describe('format tide data', function(){
   });
   it('should format tide data as JSON', function(){
       tides = new TideCrawler();
-      tides.formatToJSON(tideData).should.equal(tideDataAsJSON)
+      tides._formatToJSON(tideData).should.equal(tideDataAsJSON)
   });
 });
 
